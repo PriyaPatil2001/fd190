79b96495a0a12f3255c1464ce76dae005db18b52
PriyaPatil2001 <130456840+PriyaPatil2001@users.noreply.github.com>
2025-05-30 02:26:08 -0700

    Update Test.java

--- studentmanagementsystem/src/studentmanagementsystem/Test.java
+++ studentmanagementsystem/src/studentmanagementsystem/Test.java
@@ -1,12 +1,15 @@
 package studentmanagementsystem;
 
 public class Test {
-	public void m1() {}
+	public void m1() {
+
+		
+	}
+	public void m2(){
+
 	
+          }
 	
 
 }
-public void m2(){
 
-	
-}
